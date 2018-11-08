b3daae1d2ff3853669d91c5a9aaaeac79168ec5f
Kevin Tan <kevin.tan@liferay.com>
2018-11-08 15:58:50 +0100

    Select item defined on the query

--- src/ClayQueryRow.js
+++ src/ClayQueryRow.js
@@ -8,10 +8,36 @@ import { Config } from "metal-state";
 import templates from "./ClayQueryRow.soy.js";
 
 class ClayQueryRow extends Component {
-	created() {}
+	created() {
+		const { criteria, criteriaId, operatorId, operators } = this;
+
+		this.criteria = this.formatWithSelected(criteria, criteriaId);
+		this.operators = this.formatWithSelected(operators, operatorId);
+	}
+
+	/**
+	 * Adds a `selected` property for pre-selecting an item on the select
+	 * component.
+	 *
+	 * @param {array} list
+	 * @param {string} idSelected
+	 * @returns List of items formatted for the options property on a select
+	 * input.
+	 */
+	formatWithSelected(list, idSelected) {
+		return list.map(item => {
+			item.selected = item.value === idSelected;
+
+			return item;
+		});
+	}
 }
 
 ClayQueryRow.STATE = {
+	criteria: Config.array(),
+	criteriaId: Config.string(),
+	operatorId: Config.string(),
+	operators: Config.array(),
 	editing: Config.bool().value(false)
 };
 
