76ff4ca7bf6f141c7efc3afb6056c058fbd60feb
Angelo Youn <angelo.youn@gmail.com>
2018-11-20 13:12:23 +0100

    LPS-85743 Use React context provider to share spritemap with ClayIcon component

---
+++ src/ClayIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {PropTypes} from 'prop-types';
+import ThemeContext from './ThemeContext';
+
+class ClayIcon extends React.Component {
+    static contextType = ThemeContext;
+
+    render() {
+        const {className, iconName} = this.props;
+
+        return (
+            <svg aria-hidden="true" className={`lexicon-icon ${className}`} viewBox="0 0 512 512">
+                <use xlinkHref={ `${this.context.spriteMapPath}#${iconName}` }></use>
+            </svg>
+        );
+    }
+}
+
+ClayIcon.propTypes = {
+    className: PropTypes.string,
+    iconName: PropTypes.string,
+}
+
+export default ClayIcon;
