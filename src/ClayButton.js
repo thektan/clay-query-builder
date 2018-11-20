d8e426519add4decb5f9e503d3df686d9f7bb545
Angelo Youn <angelo.youn@gmail.com>
2018-11-20 13:12:53 +0100

    LPS-85743 Add icon support in ClayButton component

--- src/ClayButton.js
+++ src/ClayButton.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
+import ClayIcon from './ClayIcon';
+import './ClayButton.scss';
 
 class ClayButton extends React.Component {
-	render() {
-		const {label, type} = this.props;
+    render() {
+        const {className, label, iconName, ...otherProps} = this.props;
 
-		return (
-			<button className="btn btn-secondary" {...this.props}>
-				{label}
-			</button>
-		);
-	}
+        return (
+            <button className={`btn btn-secondary ${className}`} {...otherProps}>
+                {label}
+
+                {iconName && (
+                    <ClayIcon styleName={`${label ? 'text-icon' : 'icon'}`} iconName={iconName} />
+                )}
+            </button>
+        );
+    }
 }
 
 ClayButton.propTypes = {
-	label: PropTypes.string,
-	type: PropTypes.string
-};
+    className: PropTypes.string,
+    label: PropTypes.string,
+    type: PropTypes.string,
+    iconName: PropTypes.string,
+}
 
 export default ClayButton;
