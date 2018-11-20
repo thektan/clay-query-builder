d38cdcd20a4e0aec44849b708096634bd5e34ab5
Angelo Youn <angelo.youn@gmail.com>
2018-11-20 13:12:23 +0100

    LPS-85743 Use React context provider to share spritemap with ClayIcon component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClayODataQueryBuilder from './ClayODataQueryBuilder';
+import ThemeContext from './ThemeContext';
 
-export function initialize(props, id) {
-	ReactDOM.render(
-		<ClayODataQueryBuilder {...props} />,
-		document.getElementById(id)
-	);
+export function initialize(props, id, context) {
+    ReactDOM.render(
+        <ThemeContext.Provider value={context}>
+            <ClayODataQueryBuilder 
+                {...props} 
+            />
+        </ThemeContext.Provider>,
+        document.getElementById(id)
+    )
 }
