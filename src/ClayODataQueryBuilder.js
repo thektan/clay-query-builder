fc0b017bd9d220566771cb5849d8078474a3679c
Angelo Youn <angelo.youn@gmail.com>
2018-11-20 11:30:54 +0100

    LPS-85743 ODataQueryBuilder cleans up nesting; Add not equals operator

--- src/ClayODataQueryBuilder.js
+++ src/ClayODataQueryBuilder.js
@@ -31,6 +31,7 @@ const conjunctions = [
 ];
 
 const FUNCTIONAL_OPERATORS = [CONTAINS];
+const CONJUNCTIONS = [AND, OR];
 const RELATIONAL_OPERATORS = [EQ, GE, GT, LE, LT, NE];
 
 const operators = [
@@ -66,121 +67,176 @@ const operators = [
 	}
 ];
 
-const comparatorTransformation = (query, transformFromAST, prevType) => {
-	if (prevType === query.type) {
-		return [
-			...transformFromAST(query.value.left, query.type),
-			...transformFromAST(query.value.right, query.type)
-		];
-	} else {
-		return [
-			{
-				conjunctionName: oDataTransformationMap[query.type].name,
-				items: [
-					...transformFromAST(query.value.left, query.type),
-					...transformFromAST(query.value.right, query.type)
-				]
-			}
-		];
-	}
+const comparatorTransformation = ({queryAST, prevConjunction, lastNodeWasGroup}) => {
+    if (prevConjunction === queryAST.type || lastNodeWasGroup) {
+        return [
+            ...toCriteriaMap({
+                queryAST: queryAST.value.left, 
+                prevConjunction: queryAST.type,
+            }), 
+            ...toCriteriaMap({
+                queryAST: queryAST.value.right, 
+                prevConjunction: queryAST.type,
+            })
+        ];
+    }
+
+    else {
+        return toCriteriaMap(addNewGroup(queryAST, prevConjunction))
+    }
 };
 
-const groupTransformation = (query, transform) => transform(query.value);
+const addNewGroup = (queryAST, prevConjunction) => ({
+    queryAST: { value: queryAST, type: 'BoolParenExpression' }, 
+    prevConjunction, 
+    lastNodeWasGroup: false
+})
 
-const operatorTransformation = query => {
-	return [
-		{
-			operatorName: oDataTransformationMap[query.type].name,
-			propertyName: query.value.left.raw,
-			value: query.value.right.raw
-		}
-	];
+const skipGroup = (queryAST, prevConjunction) => ({
+    queryAST: queryAST.value, 
+    prevConjunction,
+    lastNodeWasGroup: true
+})
+
+const groupTransformation = ({queryAST, prevConjunction, lastNodeWasGroup}) => {
+    const nextNodeType = getNextNonGroupNodeType(queryAST);
+
+    if (lastNodeWasGroup || prevConjunction === nextNodeType || isNotConjunction(nextNodeType)) {
+        return toCriteriaMap(skipGroup(queryAST, prevConjunction));
+    }
+    else if (queryAST.value.left) {
+        const childType = getChildNodeTypeName(queryAST);
+
+        return [{
+            conjunctionName: CONJUNCTIONS.includes(childType) ? childType : AND,
+            items: [...toCriteriaMap({
+                queryAST: queryAST.value.left, 
+                prevConjunction: queryAST.type, 
+                lastNodeWasGroup: true
+            }), 
+            ...toCriteriaMap({
+                queryAST: queryAST.value.right, 
+                prevConjunction: queryAST.type, 
+                lastNodeWasGroup: true
+            })]
+        }]
+    }
+    else {
+        const childType = getChildNodeTypeName(queryAST);
+
+        return [{
+            conjunctionName: CONJUNCTIONS.includes(childType) ? childType : AND,
+            items: [...toCriteriaMap({
+                queryAST: queryAST.value, 
+                prevConjunction,
+                lastNodeWasGroup: true
+            })]
+        }]
+    }
+}
+
+const isNotConjunction = nodeType => !CONJUNCTIONS.includes(getTypeName(nodeType));
+
+const getNextNonGroupNodeType = queryAST => {
+    if (queryAST.value.type === 'BoolParentExpression') {
+        return getNextNonGroupNodeType(query.value);
+    }
+    else {
+        return queryAST.value.left ? queryAST.value.left.type : queryAST.value.type;
+    }
+}
+
+const operatorTransformation = ({queryAST}) => {
+    return [{ 
+        operatorName: oDataTransformationMap[queryAST.type].name,
+        propertyName: queryAST.value.left.raw,
+        value: queryAST.value.right.raw.replace(/['"]+/g, '')
+    }]
 };
 
-const toCriteriaMap = (queryAST, prevType) => {
-	return oDataTransformationMap[queryAST.type].transformationFunction(
-		queryAST,
-		toCriteriaMap,
-		prevType
-	);
-};
+const toCriteriaMap = ({queryAST, prevConjunction, lastNodeWasGroup = false}) => {
+    const oDataParserType = oDataTransformationMap[queryAST.type];
+    
+    return oDataParserType.transformationFunction({queryAST, prevConjunction, lastNodeWasGroup})
+}
+
+const getChildNodeTypeName = query => oDataTransformationMap[query.value.type].name;
+const getTypeName = type => oDataTransformationMap[type].name;
 
 const translateToCriteria = query => {
 	const queryAST = filter(query);
 
-	return toCriteriaMap(queryAST)[0];
-};
+    return toCriteriaMap({queryAST})[0];
+}
 
 const buildQueryString = (queryItems, queryConjunction) => {
-	let queryString = '';
+    let queryString = '';
 
-	queryItems.forEach((item, index) => {
-		const {
-			items,
-			conjunctionName,
-			propertyName,
-			operatorName,
-			value
-		} = item;
+    queryItems.forEach((item, index) => {
+        const {items, conjunctionName, propertyName, operatorName, value} = item;
 
-		if (index > 0) {
-			queryString = queryString.concat(` ${queryConjunction} `);
-		}
+        if (index > 0) {
+            queryString = queryString.concat(` ${queryConjunction} `);
+        }
 
-		if (conjunctionName) {
-			queryString = queryString.concat(
-				`(${buildQueryString(items, conjunctionName)})`
-			);
-		} else {
-			if (RELATIONAL_OPERATORS.includes(operatorName)) {
-				queryString = queryString.concat(
-					`(${propertyName} ${operatorName} "${value}")`
-				);
-			} else if (FUNCTIONAL_OPERATORS.includes(operatorName)) {
-				queryString = queryString.concat(
-					`(${operatorName} (${propertyName}, "${value}"))`
-				);
-			}
-		}
-	});
+        if (conjunctionName) {
+            queryString = queryString.concat(
+                `(${buildQueryString(items, conjunctionName)})`
+            );
+        } else {
+            if (RELATIONAL_OPERATORS.includes(operatorName)) {
+                queryString = queryString.concat(
+                    `${propertyName} ${operatorName} '${value}'`
+                );
+            } else if (FUNCTIONAL_OPERATORS.includes(operatorName)) {
+                queryString = queryString.concat(
+                    `${operatorName} (${propertyName}, '${value}')`
+                );
+            }
+        }
+    });
 
-	return queryString;
-};
+    return queryString;
+}
 
 const oDataTransformationMap = {
-	BoolParenExpression: {
-		transformationFunction: groupTransformation,
-		name: GROUP
-	},
-	OrExpression: {
-		transformationFunction: comparatorTransformation,
-		name: OR
-	},
-	AndExpression: {
-		transformationFunction: comparatorTransformation,
-		name: AND
-	},
-	GreaterOrEqualsExpression: {
-		transformationFunction: operatorTransformation,
-		name: GE
-	},
-	GreaterThanExpression: {
-		transformationFunction: operatorTransformation,
-		name: GT
-	},
-	LesserOrEqualsExpression: {
-		transformationFunction: operatorTransformation,
-		name: LE
-	},
-	LesserThanExpression: {
-		transformationFunction: operatorTransformation,
-		name: LT
-	},
-	EqualsExpression: {
-		transformationFunction: operatorTransformation,
-		name: EQ
-	}
-};
+    BoolParenExpression: {
+        transformationFunction: groupTransformation,
+        name: GROUP
+    },
+    OrExpression: {
+        transformationFunction: comparatorTransformation,
+        name: OR
+    },
+    AndExpression: {
+        transformationFunction: comparatorTransformation,
+        name: AND
+    },
+    GreaterOrEqualsExpression: {
+        transformationFunction: operatorTransformation,
+        name: GE
+    },
+    GreaterThanExpression: {
+        transformationFunction: operatorTransformation,
+        name: GT
+    },
+    LesserOrEqualsExpression: {
+        transformationFunction: operatorTransformation,
+        name: LE
+    },
+    LesserThanExpression: {
+        transformationFunction: operatorTransformation,
+        name: LT
+    },
+    EqualsExpression: {
+        transformationFunction: operatorTransformation,
+        name: EQ
+    },
+    NotEqualsExpression: {
+        transformationFunction: operatorTransformation,
+        name: NE
+    }
+}
 
 class ClayODataQueryBuilder extends React.Component {
 	constructor(props) {
